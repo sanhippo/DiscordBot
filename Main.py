e47bbee6a0eb1772bd037821a15750f8347488cf
DESKTOP-2OR9908\shawn <shawn2489@hotmail.com>
2020-07-18 11:10:30 -0400

    Fixed Rounding

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import asyncio
 import socket
 from datetime import datetime, timezone
 
-catoffset = 2
+catoffset = 3
 
 gc = gspread.service_account()
 
@@ -50,13 +50,26 @@ class Activity:  # Class for Activity Type Contains all the activity Information
 
 class RollResults:
 
-    def __init__(self, description, type, log, value, hoursused, amountconsumed):
+    def __init__(self, description, type, log, value, hoursused, amountconsumed, daysinjured):
         self.description = description
         self.type = type
         self.log = log
         self.value = value
         self.hoursused = hoursused
         self.amountconsumed = amountconsumed
+        self.daysinjured = daysinjured
+
+
+
+class Player:
+
+    def __init__(self, name, hoursused, maxhours, injury, activityvalue):
+        self.name = name
+        self.hoursused = int(hoursused)
+        self.maxhours = int(maxhours)
+        self.injury = int(injury)
+        self.activityvalue = int(activityvalue)
+        self.hoursleft = self.maxhours - self.hoursused
 
 
 
@@ -76,7 +89,7 @@ def updatecategories():  # Updates the Activity List
     z = 0  # Sets to Row 0 for each new activity
 
     for x in range(1, len(get_values[0])):
-        ActivityList.append(Activity(get_values[0][x][0], x, get_values[0][x][1], int(get_values[0][x][2]), get_values[0][x][3], get_values[0][x][4]))
+        ActivityList.append(Activity(get_values[0][x][0], x, get_values[0][x][1], get_values[0][x][2], get_values[0][x][3], get_values[0][x][4]))
 
         for y in range(5, len(get_values[0][1])):
             ActivityList[z].add_results(get_values[0][x][y])
@@ -86,17 +99,16 @@ def updatecategories():  # Updates the Activity List
     return
 
 
-def GetPlayerIndex(message):  # Determine What Player Is Making the Request Updates every request to get new data
+def GetPlayerIndex(message, collumn):  # Determine What Player Is Making the Request Updates every request to get new data
 
     PlayerList = sheetplayerinfo.col_values(1)  # Get Player Data
 
-    for x in range (len(PlayerList)):
+    for x in range(len(PlayerList)):
 
         if message.author.nick == PlayerList[x]:  # If Player Found report the value
-            player = sheetplayerinfo.row_values(x+1)
+            sheetvalues = sheetplayerinfo.row_values(x+1)
 
-            for y in range(1, len(player)):
-                player[y] = int(player[y])
+            player = Player(sheetvalues[0], sheetvalues[1], sheetvalues[2], sheetvalues[3], sheetvalues[collumn])
 
             return player
 
@@ -120,46 +132,54 @@ def GetValid(message, SelectedPlayer, SelectedCategory):
 
     del MessageContentSplit[0]
 
+    for x in range(len(SelectedCategory.expectedinputs)):
 
-    MessageLength = len(MessageContentSplit)
 
-    if MessageLength != SelectedCategory.expectedinputs:
-        return False, f"{message.author.mention} {message.content}\n Invalid Parameters. Entered: {MessageLength}, Expected: {SelectedCategory.expectedinputs}" # Invalid Number of Parameters
+        if len(SelectedCategory.expectedinputs) != len(MessageContentSplit):
 
-    for x in range(MessageLength):
-        if not RepresentsInt(MessageContentSplit[x]):
-            return False, f"{message.author.mention} {message.content}\n Input Is Not a Number" # Input is not a number
+            return False, f"{message.author.mention} {message.content}\n Invalid Number of Parameters. {len(SelectedCategory.expectedinputs)} Expected."
 
-        MessageContentSplit[x] = int(MessageContentSplit[x])
+        if SelectedCategory.expectedinputs[x].upper() == "I":
+            if not RepresentsInt(MessageContentSplit[x]):
+                return False, f"{message.author.mention} {message.content}\n Invalid Parameter ({MessageContentSplit[1]}) Should be a Number."
+            MessageContentSplit[x] = int(MessageContentSplit[x])
 
-    HoursLeft = SelectedPlayer[2] - SelectedPlayer[1]
+        if SelectedCategory.expectedinputs[x].upper() == "S":
+            MessageContentSplit[x] = MessageContentSplit[x].upper()
 
-    if ((MessageContentSplit[0] > SelectedPlayer[2]) or (MessageContentSplit[0] <= 0)):
-        return False, f"{message.author.mention} {message.content}\n Invalid Hour Entry, Must Be Between 1 and {HoursLeft}"    # Invalid Hour Entry
 
-    if MessageContentSplit[0] > HoursLeft:
-        return False, f"{message.author.mention} {message.content}\n Not Enough Hours Left In Day, Hours Left: {HoursLeft}" # Not Enough Hours Left In the Day
-    ActivityValue = SelectedPlayer[SelectedCategory.column+1]
+    if ((MessageContentSplit[0] > SelectedPlayer.maxhours) or (MessageContentSplit[0] <= 0)):
+        return False, f"{message.author.mention} {message.content}\n Invalid Hour Entry, Must Be Between 1 and {SelectedPlayer.hoursleft}"    # Invalid Hour Entry
 
-    if ActivityValue == 0:
+    if MessageContentSplit[0] > SelectedPlayer.hoursleft:
+        return False, f"{message.author.mention} {message.content}\n Not Enough Hours Left In Day, Hours Left: {SelectedPlayer.hoursleft}" # Not Enough Hours Left In the Day
+
+    if SelectedPlayer.activityvalue == 0:
         return False, f"{message.author.mention} {message.content}\n Character Does Not Meet Requirements" # Character Does Not Meet Activity Requirements
 
-    if len(MessageContentSplit) > 1:
+
+    if (SelectedCategory.style.find("c") != -1):
+
         if MessageContentSplit[1] <= 0:
-            return False, "{message.author.mention} {message.content}\n Invalid Use Amount"    # Inavlid use amount
+            return False, "{message.author.mention} {message.content}\n Invalid Use Amount"  # Invalid use amount
 
-        workamount = ActivityValue * MessageContentSplit[0]
+        workamount = SelectedPlayer.activityvalue * MessageContentSplit[0]
 
-        if (SelectedCategory.style.find("c") != -1):
-            f = SelectedCategory.style.find("c")
-            in_stock = float(sheetinfo.acell(SelectedCategory.extrainfo[f+2:f+4]).value)
+        f = SelectedCategory.style.find("c")
 
-            if workamount > in_stock:
-                return False, f"{message.author.mention} {message.content}\nNot Enough Supplies in Stock: {in_stock}" # To Large of a Number Entered For Use
+        in_stock = float(sheetinfo.acell(SelectedCategory.extrainfo[f+2:f+4]).value)
+
+        if workamount > in_stock:
+            return False, f"{message.author.mention} {message.content}\nNot Enough Supplies in Stock: {in_stock}" # To Large of a Number Entered For Use
 
         if MessageContentSplit[1] > workamount:
 
             return False, f"{message.author.mention} {message.content}\nTo Large of Amount, Max For Number of Hours Selected is {workamount}" # To Large of a Number Entered For Use
+
+    if SelectedPlayer.injury != 0:
+        if (SelectedCategory.style.find("i") == -1):
+            return False, f"{message.author.mention} {message.content}\nInjured For {SelectedPlayer.injury} days. Can't Perform Selected Activity" # To Large of a Number Entered For Use
+
 
     return True, 0  # Return True For is a Valid Case
 
@@ -177,8 +197,8 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
 
     string_name_message = f"{message.author.mention} {message.content}\n"
     dice = SelectedActivity.roll.split("d")
-    hoursleft = SelectedPlayer[2]
-    sheetdata = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    hoursleft = SelectedPlayer.hoursleft
+    sheetdata = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     # 0 = date
     # 1 = Time
     # 2 = Who
@@ -201,10 +221,13 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
 
         total = 0
         roll_values = []
+        daysinjured = 0
 
         for x in range(dice[0]):
             roll_values.append(random.randint(1, dice[1]))
             total = total + roll_values[x]
+
+        totalroll.append(total)
 
         sdescription = SelectedActivity.results[total-1]
 
@@ -215,33 +238,19 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
         status_split[3] = float(status_split[3])  # Value
         status_split[4] = int(status_split[4])  # 0 = max hours of day left
 
-        if (SelectedActivity.style.find("c") != -1):
-
-            Value = round(status_split[3] * MessageContentSplit[1], 2)
-        else:
-            Value = round(status_split[3], 2)
-
-        totalroll.append(total)
-
-
         if status_split[4] != 0:
-            hoursleft = hoursleft - status_split[4]
+            daysinjured = status_split[4]
 
         if (SelectedActivity.style.find("c") != -1):
-            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], Value, status_split[4], MessageContentSplit[1]))
 
+            value = status_split[3] * MessageContentSplit[1]
+            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], value, 1, MessageContentSplit[1], daysinjured))
         else:
-            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], Value, status_split[4], 0))
+            value = status_split[3]
+            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], value, 1, 0, daysinjured))
 
-
-        if status_split[4] == 0:
-
-            if (SelectedActivity.style.find("c") != -1):
-                rolls.append(RollResults(status_split[0], status_split[1], status_split[2], Value, hoursleft, MessageContentSplit[1]))
-            else:
-                rolls.append(RollResults(status_split[0], status_split[1], status_split[2], Value, hoursleft, 0))
+        if daysinjured != 0:
             break
-
 
     combined_results = []
 
@@ -256,9 +265,10 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
 
                 if rolls[x].type == combined_results[y].type:
                     matchfound = 1
-                    combined_results[y].value = combined_results[y].value + rolls[x].value
+                    combined_results[y].value = round(combined_results[y].value + rolls[x].value)
                     combined_results[y].hoursused = combined_results[y].hoursused + rolls[x].hoursused
                     combined_results[y].amountconsumed = combined_results[y].amountconsumed + rolls[x].amountconsumed
+                    combined_results[y].daysinjured = combined_results[y].daysinjured + rolls[x].daysinjured
 
                     break
 
@@ -290,7 +300,8 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
         sheetdata[3] = combined_results[x].description  # what
         sheetdata[4] = combined_results[x].log  # log
         sheetdata[5] = combined_results[x].value  # Value
-        sheetdata[7] = combined_results[x].hoursused
+        sheetdata[7] = combined_results[x].hoursused  # hours used
+        sheetdata[9] = combined_results[x].daysinjured
 
         sheetlog.append_row(sheetdata, value_input_option='USER_ENTERED', insert_data_option="INSERT_ROWS",
                             table_range="A1")
@@ -332,8 +343,6 @@ async def on_message(message):
 
     if message.author == client.user:
         return
-
-
 
 
     if message.content.startswith("$"):
@@ -408,17 +417,18 @@ async def on_message(message):
 
         if message.content.startswith("$"):
 
-            SelectedPlayer = GetPlayerIndex(message)
-
-            if SelectedPlayer == False:
-                await message.channel.send(f"{message.author.mention} {message.content}\n User {message.author.nick} Not Found!")
-                return
-
             SelectedActivity = GetCategory(message)
 
             if not SelectedActivity:
                 await message.channel.send(f"{message.author.mention} {message.content}\n Activity {message.content} Not Found")
                 return
+
+            SelectedPlayer = GetPlayerIndex(message, SelectedActivity.column)
+
+            if SelectedPlayer == False:
+                await message.channel.send(f"{message.author.mention} {message.content}\n User {message.author.nick} Not Found!")
+                return
+
 
             IsValid = GetValid(message, SelectedPlayer, SelectedActivity)
 
