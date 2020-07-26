9e9ffcce5233d2709d971786e1f908a82d543338
DESKTOP-2OR9908\shawn <shawn2489@hotmail.com>
2020-07-26 11:23:12 -0400

    Changed Cooking to Only one Input, Added the Status Tags

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import random
 import discord
 import gspread
-import os
 import asyncio
 import socket
 import credentials
+import math
 from datetime import datetime, timezone
 
 catoffset = 3
@@ -21,6 +21,7 @@ if Testing == 0:
     sheetlog = workbook.worksheet("Log")
     sheetplayerinfo = workbook.worksheet("Player")
     sheetinfo = workbook.worksheet("Info")
+    sheetstatus = workbook.worksheet("Status")
 
 else:
     token = credentials.TestToken  # Test Token
@@ -136,7 +137,7 @@ def GetCategory(message):  # Determine Which Category Was Called
     return False
 
 
-def GetValid(message, SelectedPlayer, SelectedCategory):
+async def GetValid(message, SelectedPlayer, SelectedCategory):
     global MessageContentSplit
 
     MessageContentSplit = message.content.split(" ")
@@ -172,9 +173,6 @@ def GetValid(message, SelectedPlayer, SelectedCategory):
 
     if (SelectedCategory.style.find("c") != -1):
 
-        if MessageContentSplit[1] <= 0:
-            return False, "{message.author.mention} {message.content}\n Invalid Use Amount"  # Invalid use amount
-
         workamount = SelectedPlayer.activityvalue * MessageContentSplit[0]
 
         f = SelectedCategory.style.find("c")
@@ -182,11 +180,13 @@ def GetValid(message, SelectedPlayer, SelectedCategory):
         in_stock = float(sheetinfo.acell(SelectedCategory.extrainfo[f+2:f+4]).value)
 
         if workamount > in_stock:
-            return False, f"{message.author.mention} {message.content}\nNot Enough Supplies in Stock: {in_stock}" # To Large of a Number Entered For Use
+            MessageContentSplit[0] = math.floor(in_stock / SelectedPlayer.activityvalue)
+            workamount == SelectedPlayer.activityvalue * MessageContentSplit[0]
 
-        if MessageContentSplit[1] > workamount:
+            if MessageContentSplit[0] == 0:
+                return False, f"{message.author.mention} {message.content}\nNot Enough Supplies. In Stock: {in_stock}"  # To Large of a Number Entered For Use
 
-            return False, f"{message.author.mention} {message.content}\nTo Large of Amount, Max For Number of Hours Selected is {workamount}" # To Large of a Number Entered For Use
+            await printdiscord(message, f"Not Enough Supplies, Hours Changed to {MessageContentSplit[0]}")
 
     if SelectedPlayer.injury != 0:
         if (SelectedCategory.style.find("i") == -1):
@@ -225,6 +225,10 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
     totalroll = []
 
 
+async def townstatus():
+    get_status = sheetstatus.batch_get(["A:C"], major_dimension="Columns")
+
+    send_string = ""
 
     for x in range(len(dice)):
         dice[x] = int(dice[x])
@@ -255,8 +259,8 @@ def GetResult(message, SelectedPlayer, SelectedActivity):
 
         if (SelectedActivity.style.find("c") != -1):
 
-            value = status_split[3] * MessageContentSplit[1]
-            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], value, 1, MessageContentSplit[1], daysinjured))
+            value = status_split[3] * SelectedPlayer.activityvalue
+            rolls.append(RollResults(status_split[0], status_split[1], status_split[2], value, 1, SelectedPlayer.activityvalue, daysinjured))
         else:
             value = status_split[3]
             rolls.append(RollResults(status_split[0], status_split[1], status_split[2], value, 1, 0, daysinjured))
@@ -349,15 +353,35 @@ def auth_and_chan(ctx):
 
     return chk
 
+    x = 1
+
+    while x < len(get_status[0][0]):
+
+        if not get_status[0][2][x] == "3":
+            send_string = send_string + "\n"
+        if get_status[0][2][x] == "0":
+            send_string = send_string + (get_status[0][1][x])
+
+        elif get_status[0][2][x] == "1":
+            tempstring = get_status[0][1][x]
+            tempstring = tempstring.replace("{Value}", get_status[0][0][x])
+            send_string = send_string + tempstring
+
+        x += 1
+
+    return send_string
+
+
+async def printdiscord(ctx, string):
+    await ctx.channel.send(string)
+    return
+
+global ActivityList
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
-global ActivityList
-
-updatecategories()
-
+    updatecategories()
 
 @client.event
 async def on_message(message):
@@ -365,8 +389,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-
     if message.content.startswith("$"):
+
+        message.content = message.content[:1] + message.content[1].upper() + message.content[2:]
+        ctx = message
 
         message.content = message.content[:1] + message.content[1].upper() + message.content[2:]
 
@@ -380,8 +406,19 @@ async def on_message(message):
             except asyncio.TimeoutError:
                 await message.channel.send("Unconfirmed. Aborting.")
 
-            return
+            channel = message.channel
 
+            await channel.send("Say Hello!")
+
+            def check(m):
+                return m.content == 'hello' and m.channel == channel
+
+            try:
+                await asyncio.wait_for(check(message), timeout=5.0)
+            except asyncio.TimeoutError:
+                print('timeout!')
+
+            return
 
         if message.content == ("$Host"):
             await message.channel.send(hostname)
@@ -398,25 +435,8 @@ async def on_message(message):
             return
 
         if message.content == ("$Status"):
-            get_status = sheetstatus.batch_get(["A:C"], major_dimension="Columns")
 
-            send_string = "**Status**:"
-
-            x = 1
-
-            while x < len(get_status[0][0]):
-                send_string = send_string + "\n"
-                if get_status[0][2][x] == "0":
-                    send_string = send_string + (get_status[0][1][x])
-
-                elif get_status[0][2][x] == "1":
-                    tempstring = get_status[0][1][x]
-                    tempstring = tempstring.replace("{Value}", get_status[0][0][x])
-                    send_string = send_string + tempstring
-
-                x += 1
-
-            await message.channel.send(send_string)
+            await printdiscord(ctx, await townstatus())
 
             return
 
@@ -459,7 +479,7 @@ async def on_message(message):
                 return
 
 
-            IsValid = GetValid(message, SelectedPlayer, SelectedActivity)
+            IsValid = await GetValid(message, SelectedPlayer, SelectedActivity)
 
             if not IsValid[0]:
                 await message.channel.send(IsValid[1])
