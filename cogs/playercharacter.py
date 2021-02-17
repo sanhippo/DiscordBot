2bf7f2a672015246b39e05b3af5936bbdc7b1e82
OAI\ssanfilippo <ssanfilippo@oai-automation.com>
2021-02-17 12:26:54 -0500

    Update GSpread

--- cogs/playercharacter.py
+++ cogs/playercharacter.py
@@ -13,6 +13,9 @@ sheet_characters = workbook.worksheet("Character")
 emoji_reroll = '<:ReRoll:806548887753457708>'  # Reroll Icon
 emoji_accept = '\N{THUMBS UP SIGN}'  # Accept Emoji
 
+strcolum = "D"
+chacolum = "I"
+
 
 # New - The Cog class must extend the commands.Cog class
 class playercharacter(commands.Cog):
@@ -43,11 +46,13 @@ class playercharacter(commands.Cog):
                 return
 
         # See if the user has a character # Todo: Needs to be improved to not be so slow try a search and select
+        rowfound = 2
         for character_data in batch_character_data:
             if str(character_data["DiscordID"]) == str(ctx.author.id):
                 break
             else:
                 character_data = None
+                rowfound += 1
         if character_data is None:
             x = 0
             while x == 0:
@@ -76,17 +81,27 @@ class playercharacter(commands.Cog):
                                             table_range="A1")
 
         while character_data["Step"] < 200:
-            roll_str = d20.roll("4d6kh3")
-            roll_dex = d20.roll("4d6kh3")
-            roll_con = d20.roll("4d6kh3")
-            roll_int = d20.roll("4d6kh3")
-            roll_wis = d20.roll("4d6kh3")
-            roll_cha = d20.roll("4d6kh3")
+            if (character_data["Step"] == 100) or (character_data["Step"] == 150):
+                roll_str = d20.roll("4d6kh3")
+                roll_dex = d20.roll("4d6kh3")
+                roll_con = d20.roll("4d6kh3")
+                roll_int = d20.roll("4d6kh3")
+                roll_wis = d20.roll("4d6kh3")
+                roll_cha = d20.roll("4d6kh3")
+            else:
+                roll_str = character_data["Str"]
+                roll_dex = character_data["Dex"]
+                roll_con = character_data["Con"]
+                roll_int = character_data["Int"]
+                roll_wis = character_data["Wis"]
+                roll_cha = character_data["Cha"]
 
             # Display Rolls
             sent_rolls = await ctx.send(f"**Str:** {roll_str}\n**Dex:** {roll_dex}\n**Con:** {roll_con}\n**Int:** {roll_int}\n**Wis:** {roll_wis}\n**Cha:** {roll_cha}")
 
-            await sent_rolls.add_reaction(emoji_reroll)
+            if character_data["Step"] == 100:  # Rolling for the First Time
+                await sent_rolls.add_reaction(emoji_reroll)
+
             await sent_rolls.add_reaction(emoji_accept)
 
             def check(reaction, user):
@@ -96,13 +111,18 @@ class playercharacter(commands.Cog):
             try:
                 reaction, user = await self.bot.wait_for('reaction_add', timeout=10.0, check=check)
             except asyncio.TimeoutError:
+                if (character_data["Step"] == 100) or (character_data["Step"] == 150):
+                    cellrange = f"{rowfound}{strcolum}:{rowfound}{chacolum}"
 
                 await ctx.send("Timeout Waiting for Selection, Please use .pc to resume in the future.")
+                await try_delete(sent_rolls)
             else:
 
                 if reaction.emoji == '👍':
+                    cellrange = f"{strcolum}{rowfound}:{chacolum}{rowfound}"
                     character_data["Step"] = 200
-
+                    character_data.update(cellrange, [roll_str, roll_dex, roll_con, roll_int, roll_wis, roll_cha])
+                    print("Done")
                 elif str(reaction.emoji) == "<:ReRoll:806548887753457708>":
                         if character_data["Step"] == 100:
                             character_data["Step"] = 150
@@ -111,7 +131,7 @@ class playercharacter(commands.Cog):
                     await ctx.send("You can't add your own reactions. Aborting, Please use .pc to resume in the future.")
 
 
-            await try_delete(sent_rolls)
+
 
                 # Give Option to Reroll
                     #Take Rolled Stats
